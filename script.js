62b189dc9c5592ad8b80e38f4e1153c3d6c14a3c
Mohammad Salman <99408156+Mohammad-Salman-Farshi@users.noreply.github.com>
2022-04-02 21:17:17 +0600

    Add files via upload
    script of javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const quizData = [
+    {
+        question: "Q : 01--> What is the rank of DIU among all private Universities of Bangladesh in QS Asia Rankings-2022?",
+        a: "1st",
+        b: "2nd",
+        c: "3rd",
+        d: "4th",
+        correct: "c",
+    },
+   
