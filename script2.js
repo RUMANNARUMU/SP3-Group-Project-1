fc7ec87164d7b5b58505a1bcf975fadefeb15b10
RUMANNARUMU <102850700+RUMANNARUMU@users.noreply.github.com>
2022-04-10 16:38:04 +0600

    Update script2.js

--- script2.js
+++ script2.js
@@ -89,3 +89,18 @@ function loadQuiz() {
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
 }
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
