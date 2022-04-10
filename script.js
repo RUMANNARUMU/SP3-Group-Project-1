45f3914c9c65e5d8fc12e717aa979f8833c007b6
JANNATUL MAOWA <102169910+JannatulMaowa-JM@users.noreply.github.com>
2022-04-10 16:12:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -95,3 +95,19 @@ function loadQuiz() {
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
 }
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
